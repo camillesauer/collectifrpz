db0301f16423fb623e93bbb99527deda188352b3
Camille Sauer <camille-sauer@hotmail.fr>
2020-09-06 15:46:00 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :categories
-  resources :avatars
-  resources :articles
+  resources :articles , only: [:index, :new, :create, :show, :update, :edit, :destroy]
+  resources :avatars, only: [:new, :create, :show, :update, :edit, :destroy]
   resources :teams, only: [:index]
+  resources :categories
   get    '/mentions'      => 'pages#mentions'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
