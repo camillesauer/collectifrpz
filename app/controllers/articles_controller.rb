e04cefd29220df5abfea82bbce8966d45a66642a
Camille Sauer <camille-sauer@hotmail.fr>
2020-09-06 15:37:02 +0200

    duplicated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,74 +1,45 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
-
-  # GET /articles
-  # GET /articles.json
   def index
-    @articles = Article.all
+   @article =  Article.order(id: :desc)
+
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
-  def show
-  end
-
-  # GET /articles/new
   def new
-    @article = Article.new
+    @article = Article.new # needed to instantiate the form_for
   end
 
-  # GET /articles/1/edit
-  def edit
-  end
-
-  # POST /articles
-  # POST /articles.json
   def create
     @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save!
+    redirect_to articles_path(@article)
     end
+
+    # Will raise ActiveModel::ForbiddenAttributesError
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to article_path(@article)
   end
+  def show
+    @article = Article.find(params[:id])
+ end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
   def destroy
+    @article = Article.find(params[:id])
     @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to articles_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.fetch(:article, {})
-    end
+  def article_params
+    params.require(:article).permit(:title, :sous_titre, :lieu, :date, :image_url, :content, :image_url_2, :content_2, :image_url_3, :content_3, :image_url_4, :content_4, :image_url_5, :content_5, :image_url_6, :image_url_7, :image_url_8, :image_url_9, :image_url_10, :legend, :legend_1, :legend_2, :legend_3, :legend_4, :legend_5, :legend_6, :legend_7, :legend_8, :legend_9, :legend_10, :video)
+  end
 end
